c52225987b2af3ca23fb03745d6034992e0bb5e1
Thales Cristian <thales.cristianeugenio@gmail.com>
2023-09-05 11:47:20 -0300

    Start

---
+++ src/lib/main.dart
@@ -0,0 +1,16 @@
+import 'package:flutter/material.dart';
+
+void main() {
+  runApp(Container(
+    color: Color.fromRGBO(47, 53, 67, 1.0),
+    child: Center(
+      child: Text(
+        'AAAAAAAAA',
+        textDirection: TextDirection.ltr,
+        style: TextStyle(
+          color: Colors.white,
+        ),
+      ),
+    ),
+  ));
+}
